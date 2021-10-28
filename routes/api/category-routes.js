ca2d4fa915cc87f56d3b5dcc3ce88e1a0294b69b
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-27 22:17:44 -0400

    adding put route to update existing category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,11 +55,19 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update(
-
+    const categoryData = await Category.update( { category_name: req.body.category_name }, { where: { id: req.params.id } }
     )
-  } catch {
 
+    // handle case where category id doesn't exist
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+    
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
