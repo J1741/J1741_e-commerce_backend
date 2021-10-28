05c3b5d72ef262d769792d1f2838d0d18f21ea1d
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-27 21:33:18 -0400

    adding get route for single category and product info

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,9 +17,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne({ 
+      where: { id: req.params.id},
+      include: [{
+        model: Product
+      }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
