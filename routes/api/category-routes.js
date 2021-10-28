4f1e3e61588bbc91ef97992e8fa413a50f69b330
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-27 22:03:19 -0400

    addding delete route to category_routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,12 +52,35 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+
+    )
+  } catch {
+
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { 
+        id: req.params.id
+      }
+    })
+
+    // handle case where category id doesn't exist
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
