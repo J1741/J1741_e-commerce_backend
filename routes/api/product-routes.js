55cd6548d7df269b16abec018d21ec4c25f47ece
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-29 15:36:40 -0400

    adding route to get one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,9 +21,30 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  // new stuff
+  try {
+    const productData = await Product.findOne({ 
+      where: { 
+        id: req.params.id
+      },
+      include: [Category, {model: Tag, through: ProductTag}]
+    });
+
+    // handle case where category id doesn't exist
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+
+    res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
