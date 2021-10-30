06c02eed2204dfc866841c7eaaf076c6776ee234
J1741 <84889065+J1741@users.noreply.github.com>
2021-10-30 15:39:51 -0400

    adding route to get single tag and associated product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,9 +20,30 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{
+        model: Product, through: ProductTag
+      }]
+    });
+
+    // handle case where tag id doesn't exist
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+
+  } catch {
+      res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
